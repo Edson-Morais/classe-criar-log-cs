a339cf88c41a7de77772df14f621342993435659
Edson Morais <88722547+Edson-Morais@users.noreply.github.com>
2021-09-03 11:46:40 -0300

    Programa visual studio

---
+++ Log.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_criar_LOG
+{
+    public class Log
+    {
+        static string pastausuario = Environment.GetEnvironmentVariable("HOMEPATH");// caminho padrão pasta usuário
+        string diretorio = pastausuario + @"\Documents\";// caminho pasta documentos
+        string nomePasta = "LOG";//nome da pasta
+        string nomeArquivo = "log.txt";// nome do arquivo
+
+
+        public Log()
+        {
+            
+        }
+        public void criar(string log)
+        {
+            FileInfo file = new FileInfo(diretorio+"\\"+nomePasta);// passa o diretorio da pasta log
+            //confere se já existe a pasta
+            if (file.Exists)
+            {               
+                StreamWriter arquivo = new StreamWriter(diretorio + "\\" + nomePasta + "\\" + nomeArquivo, true);
+                arquivo.WriteLine(log + " " + DateTime.Now.ToString());
+                arquivo.Close();
+            }
+            else
+            {
+                DirectoryInfo raiz = new DirectoryInfo(diretorio);
+                raiz.CreateSubdirectory(nomePasta);
+                StreamWriter arquivo = new StreamWriter(diretorio + "\\" + nomePasta + "\\" + nomeArquivo, true);
+                arquivo.WriteLine(log + " " + DateTime.Now.ToString());
+                arquivo.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Classe_criar_LOG
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Log log = new Log();
+            for (int i = 0; i < 2000; i++)
+            {
+                log.criar(i.ToString());
+            }
+            Console.WriteLine("arquivo criado ");
+            Console.ReadLine();
+        }
+    }
+}
